234cd9f126e2e1f817873336785d8af1b22f0861
wise-toddler <wiseeldrich2004@gmail.com>
2024-09-13 03:43:44 +0530

    Refactor DataTable component to add sorting functionality

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,11 +1,12 @@
-import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
-import { Pencil, Trash2 } from 'lucide-react';
+import React, { useMemo, useState } from 'react';
+import { useTable, useSortBy } from 'react-table';
+import { Pencil, Trash2, ChevronUp, ChevronDown } from 'lucide-react';
 import './../../styles/DataTable.css';
 import { useAuth } from '../context/AuthContext';
 
 const DataTable = React.memo(({ columns, data, onEdit, onDelete }) => {
-  const {isAdmin} = useAuth();
+  const { isAdmin } = useAuth();
+  const [sortBy, setSortBy] = useState([]);
 
   const actionColumn = useMemo(() => ({
     Header: 'Actions',
@@ -24,13 +25,21 @@ const DataTable = React.memo(({ columns, data, onEdit, onDelete }) => {
   const tableColumns = useMemo(() => {
     return isAdmin ? [...columns, actionColumn] : columns;
   }, [columns, actionColumn, isAdmin]);
+
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
-  } = useTable({ columns: tableColumns, data });
+  } = useTable(
+    {
+      columns: tableColumns,
+      data,
+      initialState: { sortBy }
+    },
+    useSortBy
+  );
 
   return (
     <table {...getTableProps()} className="data-table">
@@ -38,7 +47,16 @@ const DataTable = React.memo(({ columns, data, onEdit, onDelete }) => {
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render('Header')}
+                <span>
+                  {column.isSorted
+                    ? column.isSortedDesc
+                      ? <ChevronDown size={14} />
+                      : <ChevronUp size={14} />
+                    : ''}
+                </span>
+              </th>
             ))}
           </tr>
         ))}
@@ -47,9 +65,9 @@ const DataTable = React.memo(({ columns, data, onEdit, onDelete }) => {
         {rows.map(row => {
           prepareRow(row);
           return (
-            <tr key={row.id} {...row.getRowProps()}>
+            <tr {...row.getRowProps()}>
               {row.cells.map(cell => {
-                return <td key={cell.column.id} {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
               })}
             </tr>
           );
